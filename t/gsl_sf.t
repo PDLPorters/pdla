PDLPorters/pdl

Load all GSLSF modules in t/gsl_sf.t; add 2 test for Legendre module.
At least by loading the modules we will get a test failure later if
some function we provide bindings for has been deprecated.

t/gsl_sf.t
@@ -3,33 +3,68 @@
 # Test Script for the PDL interface to the GSL library
 #  This tests only that the interface is working, i.e. that the
 #   functions can be called. The actual return values are not
-#   checked. 
+#   checked.
 #  The GSL library already has a extensive test suite, and we
 #  do not want to duplicate that effort here.
 
 use PDL::LiteF;
 use Test::More;
+use strict;
 
 BEGIN
 {
    use PDL::Config;
    if ( $PDL::Config{WITH_GSL} ) {
-      eval " use PDL::GSLSF::BESSEL; ";
+      eval {
+	  use PDL::GSLSF::AIRY;
+	  use PDL::GSLSF::BESSEL;
+	  use PDL::GSLSF::CLAUSEN;
+	  use PDL::GSLSF::COULOMB;
+	  use PDL::GSLSF::COUPLING;
+	  use PDL::GSLSF::DAWSON;
+	  use PDL::GSLSF::DEBYE;
+	  use PDL::GSLSF::DILOG;
+	  use PDL::GSLSF::ELEMENTARY;
+	  use PDL::GSLSF::ELLINT;
+	  use PDL::GSLSF::ELLJAC;
+	  use PDL::GSLSF::ERF;
+	  use PDL::GSLSF::EXP;
+	  use PDL::GSLSF::EXPINT;
+	  use PDL::GSLSF::FERMI_DIRAC;
+	  use PDL::GSLSF::GAMMA;
+	  use PDL::GSLSF::GEGENBAUER;
+	  use PDL::GSLSF::HYPERG;
+	  use PDL::GSLSF::LAGUERRE;
+	  use PDL::GSLSF::LEGENDRE;
+	  use PDL::GSLSF::LOG;
+	  use PDL::GSLSF::POLY;
+	  use PDL::GSLSF::POW_INT;
+	  use PDL::GSLSF::PSI;
+	  use PDL::GSLSF::SYNCHROTRON;
+	  use PDL::GSLSF::TRANSPORT;
+	  use PDL::GSLSF::TRIG;
+	  use PDL::GSLSF::ZETA;
+      };
       unless ($@) {
-         plan tests => 1;
+         plan tests => 3;
       } else {
-         plan skip_all => "PDL::GSLSF::BESSEL not installed.";
+	  warn "Warning: $@\n\n";
+         plan skip_all => "PDL::GSLSF modules not installed.";
       }
    } else {
-      plan skip_all => "PDL::GSLSF::BESSEL not compiled.";
+      plan skip_all => "PDL::GSLSF modules not compiled.";
    }
 }
 
-$arg = 5.0;
-$expected = -0.17759677131433830434739701;
+my $arg = 5.0;
+my $expected = -0.17759677131433830434739701;
 
-($y,$err) = gsl_sf_bessel_Jn $arg, 0;
+my ($y,$err) = gsl_sf_bessel_Jn($arg, 0);
 
-print "got $y +- $err\n";
+#print "got $y +- $err\n";
 
-ok abs($y-$expected) < 1e-6;
+ok(abs($y-$expected) < 1e-6,"GSL SF Bessel function");
+
+my $Ylm = gsl_sf_legendre_array(xvals(21)/10-1,'Y',4,-1);
+ok($Ylm->slice("(0)")->uniq->nelem == 1, "Legendre Y00 is constant");
+ok(approx($Ylm->slice("(0),(0)"),0.5/sqrt(3.141592654),1E-6), "Y00 value is corect");

PDLPorters/pdla

t/gsl_sf.t
@@ -3,33 +3,68 @@
 # Test Script for the PDLA interface to the GSL library
 #  This tests only that the interface is working, i.e. that the
 #   functions can be called. The actual return values are not
-#   checked. 
+#   checked.
 #  The GSL library already has a extensive test suite, and we
 #  do not want to duplicate that effort here.
 
 use PDLA::LiteF;
 use Test::More;
+use strict;
 
 BEGIN
 {
    use PDLA::Config;
    if ( $PDLA::Config{WITH_GSL} ) {
-      eval " use PDLA::GSLSF::BESSEL; ";
+      eval {
+	  use PDLA::GSLSF::AIRY;
+	  use PDLA::GSLSF::BESSEL;
+	  use PDLA::GSLSF::CLAUSEN;
+	  use PDLA::GSLSF::COULOMB;
+	  use PDLA::GSLSF::COUPLING;
+	  use PDLA::GSLSF::DAWSON;
+	  use PDLA::GSLSF::DEBYE;
+	  use PDLA::GSLSF::DILOG;
+	  use PDLA::GSLSF::ELEMENTARY;
+	  use PDLA::GSLSF::ELLINT;
+	  use PDLA::GSLSF::ELLJAC;
+	  use PDLA::GSLSF::ERF;
+	  use PDLA::GSLSF::EXP;
+	  use PDLA::GSLSF::EXPINT;
+	  use PDLA::GSLSF::FERMI_DIRAC;
+	  use PDLA::GSLSF::GAMMA;
+	  use PDLA::GSLSF::GEGENBAUER;
+	  use PDLA::GSLSF::HYPERG;
+	  use PDLA::GSLSF::LAGUERRE;
+	  use PDLA::GSLSF::LEGENDRE;
+	  use PDLA::GSLSF::LOG;
+	  use PDLA::GSLSF::POLY;
+	  use PDLA::GSLSF::POW_INT;
+	  use PDLA::GSLSF::PSI;
+	  use PDLA::GSLSF::SYNCHROTRON;
+	  use PDLA::GSLSF::TRANSPORT;
+	  use PDLA::GSLSF::TRIG;
+	  use PDLA::GSLSF::ZETA;
+      };
       unless ($@) {
-         plan tests => 1;
+         plan tests => 3;
       } else {
-         plan skip_all => "PDLA::GSLSF::BESSEL not installed.";
+	  warn "Warning: $@\n\n";
+         plan skip_all => "PDLA::GSLSF modules not installed.";
       }
    } else {
-      plan skip_all => "PDLA::GSLSF::BESSEL not compiled.";
+      plan skip_all => "PDLA::GSLSF modules not compiled.";
    }
 }
 
-$arg = 5.0;
-$expected = -0.17759677131433830434739701;
+my $arg = 5.0;
+my $expected = -0.17759677131433830434739701;
 
-($y,$err) = gsl_sf_bessel_Jn $arg, 0;
+my ($y,$err) = gsl_sf_bessel_Jn($arg, 0);
 
-print "got $y +- $err\n";
+#print "got $y +- $err\n";
 
-ok abs($y-$expected) < 1e-6;
+ok(abs($y-$expected) < 1e-6,"GSL SF Bessel function");
+
+my $Ylm = gsl_sf_legendre_array(xvals(21)/10-1,'Y',4,-1);
+ok($Ylm->slice("(0)")->uniq->nelem == 1, "Legendre Y00 is constant");
+ok(approx($Ylm->slice("(0),(0)"),0.5/sqrt(3.141592654),1E-6), "Y00 value is corect");