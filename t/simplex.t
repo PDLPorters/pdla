navoj/pdl

test cleanup: t/simplex.t

t/simplex.t
@@ -1,19 +1,15 @@
 use PDL::LiteF;
-use Test;
+use Test::More;
 use strict;
 use warnings;
 
-BEGIN {
-    eval "use PDL::Opt::Simplex;";
-    unless ($@) {
-        plan tests => 8;
-    }       
-    else {
-        plan tests => 1;
-        print "ok 1 # Skipped: PDL::Opt::Simplex not installed\n";
-        exit;
-    }
-}
+eval {
+	require PDL::Opt::Simplex;
+	PDL::Opt::Simplex->import();
+	1;
+} or plan skip_all => "PDL::Opt::Simplex not installed: $@";
+
+plan tests => 8;
 
 my $dis = pdl( 0, 1 );
 my $func = sub {
@@ -21,31 +17,32 @@ my $func = sub {
     return sumover( ( $_[0] - $dis )**2 ) + 1;
 };
 
-my ( $opt, $ssize, $optval );
-my ( $x, $y );
-
+{
 # first test
-( $opt, $ssize, $optval ) = PDL::Opt::Simplex::simplex( 
+my ( $opt, $ssize, $optval ) = PDL::Opt::Simplex::simplex( 
     pdl( [ 2, 2 ] ), pdl( [ 0.01, 0.01 ] ), 1e-4, 1e4, $func,
 );
 
-( $x, $y ) = $opt->list;
+my ( $x, $y ) = $opt->list;
 
 ok( $x < 0.001 );
 ok( ( $y - 1 ) < 0.001 );
 ok( $ssize < 0.001 );
 ok( ( $optval - 1 ) < 0.001 ); 
+}
 
+{
 # second test
 my $logsub = sub {};
-( $opt, $ssize, $optval ) = PDL::Opt::Simplex::simplex( 
+my ( $opt, $ssize, $optval ) = PDL::Opt::Simplex::simplex( 
     pdl( [ [ -1, -1 ], [ -1.1, -1 ], [ -1.1, -0.9 ] ] ), pdl( [ 0.01, 0.01 ] ),
     1e-4, 1e4, $func,
 );
 
-( $x, $y ) = $opt->list;
+my ( $x, $y ) = $opt->list;
 
 ok( $x < 0.001 );
 ok( ( $y - 1 ) < 0.001 );
 ok( $ssize < 0.001 );
 ok( ( $optval - 1 ) < 0.001 ); 
+}

PDLPorters/pdla

t/simplex.t
@@ -1,19 +1,15 @@
 use PDLA::LiteF;
-use Test;
+use Test::More;
 use strict;
 use warnings;
 
-BEGIN {
-    eval "use PDLA::Opt::Simplex;";
-    unless ($@) {
-        plan tests => 8;
-    }
-    else {
-        plan tests => 1;
-        print "ok 1 # Skipped: PDLA::Opt::Simplex not installed\n";
-        exit;
-    }
-}
+eval {
+	require PDLA::Opt::Simplex;
+	PDLA::Opt::Simplex->import();
+	1;
+} or plan skip_all => "PDLA::Opt::Simplex not installed: $@";
+
+plan tests => 8;
 
 my $dis = pdl( 0, 1 );
 my $func = sub {
@@ -21,31 +17,32 @@ my $func = sub {
     return sumover( ( $_[0] - $dis )**2 ) + 1;
 };
 
-my ( $opt, $ssize, $optval );
-my ( $x, $y );
-
+{
 # first test
-( $opt, $ssize, $optval ) = PDLA::Opt::Simplex::simplex(
+my ( $opt, $ssize, $optval ) = PDLA::Opt::Simplex::simplex(
     pdl( [ 2, 2 ] ), pdl( [ 0.01, 0.01 ] ), 1e-4, 1e4, $func,
 );
 
-( $x, $y ) = $opt->list;
+my ( $x, $y ) = $opt->list;
 
 ok( $x < 0.001 );
 ok( ( $y - 1 ) < 0.001 );
 ok( $ssize < 0.001 );
 ok( ( $optval - 1 ) < 0.001 );
+}
 
+{
 # second test
 my $logsub = sub {};
-( $opt, $ssize, $optval ) = PDLA::Opt::Simplex::simplex(
+my ( $opt, $ssize, $optval ) = PDLA::Opt::Simplex::simplex(
     pdl( [ [ -1, -1 ], [ -1.1, -1 ], [ -1.1, -0.9 ] ] ), pdl( [ 0.01, 0.01 ] ),
     1e-4, 1e4, $func,
 );
 
-( $x, $y ) = $opt->list;
+my ( $x, $y ) = $opt->list;
 
 ok( $x < 0.001 );
 ok( ( $y - 1 ) < 0.001 );
 ok( $ssize < 0.001 );
 ok( ( $optval - 1 ) < 0.001 );
+}