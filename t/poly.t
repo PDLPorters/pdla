navoj/pdl

test cleanup: t/poly.t

t/poly.t
@@ -1,20 +1,25 @@
 use Test::More;
 use PDL::LiteF;
-BEGIN {
-        eval " use PDL::Fit::Polynomial; ";
-        plan skip_all => "PDL::Fit::Polynomial: $@" if $@;
-}
 
-kill INT,$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
+use strict;
+use warnings;
+
+eval {
+	require PDL::Fit::Polynomial;
+	PDL::Fit::Polynomial->import();
+	1;
+} or plan skip_all => "PDL::Fit::Polynomial: $@";
+
+kill 'INT',$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 plan tests => 1;
 
-$x = sequence(20)-10;
-$y = 30-2*$x+3*$x**2-2*$x**3;
+my $x = sequence(20)-10;
+my $y = 30-2*$x+3*$x**2-2*$x**3;
 
 # Random numbers, generated by  grandom($y)*100
 # Hard-wired to avoid OS seed variations barfing test
 
-$rand = pdl qw/65.735917 -40.510143 -122.07767 -19.591344 -139.76362  
+my $rand = pdl qw/65.735917 -40.510143 -122.07767 -19.591344 -139.76362  
 106.44639 -0.30094068 -5.3129683  49.815455  97.247868 -9.3130775  
 19.585472  8.5260268 -194.49596  73.822799  25.628967  133.36015 -2.6611465  
 9.0335632 -0.82946383/;
@@ -23,7 +28,7 @@ $y += $rand;
 
 #points $x,$y;
 
-$yfit = fitpoly1d($x,$y,4);
+my $yfit = fitpoly1d($x,$y,4);
 
 #hold; line $x, $yfit;
 

PDLPorters/pdla

t/poly.t
@@ -4,13 +4,11 @@ use PDLA::LiteF;
 use strict;
 use warnings;
 
-BEGIN {
-	eval {
-		require PDLA::Fit::Polynomial;
-		PDLA::Fit::Polynomial->import();
-		1;
-	} or plan skip_all => "PDLA::Fit::Polynomial: $@";
-}
+eval {
+	require PDLA::Fit::Polynomial;
+	PDLA::Fit::Polynomial->import();
+	1;
+} or plan skip_all => "PDLA::Fit::Polynomial: $@";
 
 kill 'INT',$$ if $ENV{UNDER_DEBUGGER}; # Useful for debugging.
 plan tests => 1;
@@ -21,7 +19,7 @@ my $y = 30-2*$x+3*$x**2-2*$x**3;
 # Random numbers, generated by  grandom($y)*100
 # Hard-wired to avoid OS seed variations barfing test
 
-my $rand = pdl qw/65.735917 -40.510143 -122.07767 -19.591344 -139.76362
+my $rand = pdl qw/65.735917 -40.510143 -122.07767 -19.591344 -139.76362  
 106.44639 -0.30094068 -5.3129683  49.815455  97.247868 -9.3130775  
 19.585472  8.5260268 -194.49596  73.822799  25.628967  133.36015 -2.6611465  
 9.0335632 -0.82946383/;