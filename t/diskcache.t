navoj/pdl

test cleanup: t/diskcache.t

t/diskcache.t
@@ -1,47 +1,41 @@
 
 use strict;
+use warnings;
 
 use PDL;
 use PDL::Config;
 use File::Temp 'tempdir';
 use File::Spec;
 
+use Test::More tests => 4;
+use Test::Exception;
+
 # Temp directory name.  The catfile() call adds a trailing dir
 # separator (e.g. "/" on POSIX).
 my $d = File::Spec->catfile(tempdir(CLEANUP=>1),"");
 
-use Test;
-BEGIN { plan tests => 4; }
-
 ##1 Make sure the library loads
 
-eval 'use PDL::DiskCache;';
-if($@) {print $@,"\n";}
-ok( !$@ );
+use PDL::DiskCache;
 
 ##2 Make a DiskCache object
 
 
-eval <<'BAR'
-  do {
-    my($a) = diskcache(["${d}1","${d}2","${d}3"],{verbose=>1});
-    $a->[0] = zeroes(10,10);
-    $a->[1] = xvals(10,10);
-    $a->[2] = yvals(10,10);
-  } while(0);
-BAR
-  ;
-ok( !$@ );
+lives_ok {
+  my($pa) = diskcache(["${d}1","${d}2","${d}3"],{verbose=>1});
+  $pa->[0] = zeroes(10,10);
+  $pa->[1] = xvals(10,10);
+  $pa->[2] = yvals(10,10);
+  1;
+};
 
 ok( (-e "${d}1") && (-e "${d}2") && (-e "${d}3") );
 
-eval <<'BAZ'
-  do {
-    my($b) = diskcache(["${d}1","${d}2","${d}3"],{ro=>1});
-    ok( ($b->[0]->sum == 0) && ($b->[1]->sum == xvals(10,10)->sum) );
-  }
-BAZ
-  ;
+my $pb;
+lives_ok {
+  ($pb) = diskcache(["${d}1","${d}2","${d}3"],{ro=>1});
+};
+ok( ($pb->[0]->sum == 0) && ($pb->[1]->sum == xvals(10,10)->sum) );
 
 
 # end

PDLPorters/pdla

t/diskcache.t
@@ -5,35 +5,35 @@ use PDLA::Config;
 use File::Temp 'tempdir';
 use File::Spec;
 
+use Test::More tests => 4;
+use Test::Exception;
+
 # Temp directory name.  The catfile() call adds a trailing dir
 # separator (e.g. "/" on POSIX).
 my $d = File::Spec->catfile(tempdir(CLEANUP=>1),"");
 
-use Test::More tests => 4;
+##1 Make sure the library loads
 
-eval 'use PDLA::DiskCache;';
-ok( !$@, "use PDLA::DiskCache succeeds" ) or diag($@);
+use PDLA::DiskCache;
 
 ## Make a DiskCache object
 ##exercises STORE, sync, and DESTROY
 
-eval <<'BAR'
-  do {
-    my($a) = diskcache(["${d}1","${d}2","${d}3"],{verbose=>1});
-    $a->[0] = zeroes(10,10);
-    $a->[1] = xvals(10,10);
-    $a->[2] = yvals(10,10);
-  } while(0);
-BAR
-  ;
-ok( !$@, "Make a DiskCache object") or diag($@);
+lives_ok {
+  my($pa) = diskcache(["${d}1","${d}2","${d}3"],{verbose=>1});
+  $pa->[0] = zeroes(10,10);
+  $pa->[1] = xvals(10,10);
+  $pa->[2] = yvals(10,10);
+  1;
+}, "Make a DiskCache object";
 
 ok( (-e "${d}1") && (-e "${d}2") && (-e "${d}3"), "3 files written");
 
-eval <<'BAZ'
-  do {
-    my($b) = diskcache(["${d}1","${d}2","${d}3"],{ro=>1});
-    ok( ($b->[0]->sum == 0) && ($b->[1]->sum == xvals(10,10)->sum), "files read correctly");
-  }
-BAZ
-  ;
+my $pb;
+lives_ok {
+  ($pb) = diskcache(["${d}1","${d}2","${d}3"],{ro=>1});
+};
+ok( ($pb->[0]->sum == 0) && ($pb->[1]->sum == xvals(10,10)->sum) );
+
+
+# end