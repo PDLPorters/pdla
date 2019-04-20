PDLPorters/pdl

Convert a bunch more tests to Test::More.

t/lut.t
@@ -1,11 +1,7 @@
 # -*-perl-*-
-
 use strict;
-use Test;
-
-BEGIN {
-    plan tests => 8;
-}
+use warnings;
+use Test::More tests => 8;
 
 use PDL::LiteF;
 use PDL::Types;
@@ -14,27 +10,27 @@ use PDL::Graphics::LUT;
 sub tapprox {
     my($a,$b) = @_;
     my $d = max( abs($a-$b) );
-    $d < 0.0001;	
+    $d < 0.0001;
 }
 
 my @names = lut_names();
-ok( $#names > -1, 1 );  # 1
+is( $#names > -1, 1 , "lut_names returns non-empty list");
 
 my @cols = lut_data( $names[0] );
-ok( $#cols, 3 );                         # 2
-ok( $cols[0]->nelem, $cols[1]->nelem );  # 3
-ok( $cols[2]->get_datatype, $PDL_F );    # 4
+is( $#cols, 3, "lut_data returns 4 columns");
+is( $cols[0]->nelem, $cols[1]->nelem, "equal number of elements in cols 0 and 1");
+is( $cols[2]->get_datatype, $PDL_F, "datatype of col 2 is float");
 
 # check we can reverse things
 my @cols2 = lut_data( $names[0], 1 );
-ok( tapprox($cols[3]->slice('-1:0'),$cols2[3]), 1 );  # 5
+ok( tapprox($cols[3]->slice('-1:0'),$cols2[3]), "reverse lut works");
 
 # check we know about the intensity ramps
 my @ramps = lut_ramps();
-ok( $#ramps > -1, 1 ); # 6
+is( $#ramps > -1, 1, "lut_ramps returns some ramps");
 
 # load in a different intensity ramp
-my @cols3 = lut_data( $names[0], 0, $ramps[0] ); 
-ok( $cols3[0]->nelem, $cols3[1]->nelem ); # 7
-ok( tapprox($cols[1],$cols3[1]), 1 );      # 8
+my @cols3 = lut_data( $names[0], 0, $ramps[0] );
+is( $cols3[0]->nelem, $cols3[1]->nelem, "intensity ramp nelem check");
+is( tapprox($cols[1],$cols3[1]), 1, "intensity ramp vals check");
 

PDLPorters/pdla

t/lut.t
@@ -1,5 +1,4 @@
 # -*-perl-*-
-
 use strict;
 use warnings;
 use Test::More tests => 8;
@@ -8,30 +7,30 @@ use PDLA::LiteF;
 use PDLA::Types;
 use PDLA::Graphics::LUT;
 
+sub tapprox {
+    my($a,$b) = @_;
+    my $d = max( abs($a-$b) );
+    $d < 0.0001;
+}
+
 my @names = lut_names();
-isnt scalar(@names), 0;  # 1
+is( $#names > -1, 1 , "lut_names returns non-empty list");
 
 my @cols = lut_data( $names[0] );
-is( scalar(@cols), 4 );                         # 2
-is( $cols[0]->nelem, $cols[1]->nelem );  # 3
-is( $cols[2]->get_datatype, $PDLA_F );    # 4
+is( $#cols, 3, "lut_data returns 4 columns");
+is( $cols[0]->nelem, $cols[1]->nelem, "equal number of elements in cols 0 and 1");
+is( $cols[2]->get_datatype, $PDLA_F, "datatype of col 2 is float");
 
-TODO: {
-local $TODO = 'Fragile test';
 # check we can reverse things
 my @cols2 = lut_data( $names[0], 1 );
-ok( all approx($cols[3]->slice('-1:0'),$cols2[3]) );  # 5
-}
+ok( tapprox($cols[3]->slice('-1:0'),$cols2[3]), "reverse lut works");
 
 # check we know about the intensity ramps
 my @ramps = lut_ramps();
-isnt scalar(@ramps), 0; # 6
+is( $#ramps > -1, 1, "lut_ramps returns some ramps");
 
 # load in a different intensity ramp
-my @cols3 = lut_data( $names[0], 0, $ramps[0] ); 
-is( $cols3[0]->nelem, $cols3[1]->nelem ); # 7
+my @cols3 = lut_data( $names[0], 0, $ramps[0] );
+is( $cols3[0]->nelem, $cols3[1]->nelem, "intensity ramp nelem check");
+is( tapprox($cols[1],$cols3[1]), 1, "intensity ramp vals check");
 
-TODO: {
-local $TODO = 'Fragile test';
-ok( all approx($cols[1],$cols3[1]) );      # 8
-}