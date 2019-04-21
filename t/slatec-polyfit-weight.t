PDLPorters/pdl

Use Config{WITH_SLATEC} in slatec-polyfit-weight.t startup
This makes the test planning consistent between the two slatec
test programs.

t/slatec-polyfit-weight.t
@@ -13,14 +13,18 @@ use warnings;
 ## <https://github.com/PDLPorters/pdl/issues/48>
 
 use PDL::LiteF;
+use PDL::Config;
+
 BEGIN {
-	eval {
+    unless ($PDL::Config{WITH_SLATEC} &&
+	    eval {
 		require PDL::Slatec;
 		PDL::Slatec->import();
 		1;
-	} or do {
-		plan skip_all => "PDL::Slatec not available";
-	}
+	    }
+	) {
+	plan skip_all => "PDL::Slatec not available";
+    }
 }
 
 plan tests => 3;

PDLPorters/pdla

t/slatec-polyfit-weight.t
@@ -13,14 +13,18 @@ use warnings;
 ## <https://github.com/PDLPorters/pdl/issues/48>
 
 use PDLA::LiteF;
+use PDLA::Config;
+
 BEGIN {
-	eval {
+    unless ($PDLA::Config{WITH_SLATEC} &&
+	    eval {
 		require PDLA::Slatec;
 		PDLA::Slatec->import();
 		1;
-	} or do {
-		plan skip_all => "PDLA::Slatec not available";
-	}
+	    }
+	) {
+	plan skip_all => "PDLA::Slatec not available";
+    }
 }
 
 plan tests => 3;